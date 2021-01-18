bb2da0bf0be6cb920d709820a3fa50f015d82f26
AlexeyQian <alexeyqian@gmail.com>
2021-01-18 16:13:09 +0700

    add behaviors

---
+++ Office/Behaviors/BindableBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Office.Behaviors
+{
+    public class BindableBehavior<T> : Behavior<T> where T: BindableObject
+    {
+        public T AssociatedObject { get; private set; }
+
+        protected override void OnAttachedTo(T visualElement)
+        {
+            base.OnAttachedTo(visualElement);
+
+            AssociatedObject = visualElement;
+            if(visualElement.BindingContext != null)
+            {
+                BindingContext = visualElement.BindingContext;
+            }
+
+            visualElement.BindingContextChanged += OnBindingContextChanged;
+        }
+
+        private void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = AssociatedObject.BindingContext;
+        }
+
+        protected override void OnDetachingFrom(T bindable)
+        {
+           view.BindingContextChanged -= OnBindingContextChanged;
+        }
+
+    }
+}
